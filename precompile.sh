0bb7f5f47e12ddaed69a23085bda15d5904a3e95
Tong Shu Li <tongli@scripps.edu>
2017-01-20 01:21:54 +0000

    Run anatomy information preprocessor

--- precompile.sh
+++ precompile.sh
@@ -1,3 +1,3 @@
-#jupyter nbconvert --execute compile/AeG-expression.ipynb --inplace --FilesWriter.build_directory=compile --ExecutePreprocessor.timeout=-1
+jupyter nbconvert --execute compile/AeG-expression.ipynb --inplace --FilesWriter.build_directory=compile --ExecutePreprocessor.timeout=-1
 jupyter nbconvert --execute compile/DaG-association.ipynb --inplace --FilesWriter.build_directory=compile --ExecutePreprocessor.timeout=-1
 jupyter nbconvert --execute compile/CbG-binding.ipynb --inplace --FilesWriter.build_directory=compile --ExecutePreprocessor.timeout=-1
